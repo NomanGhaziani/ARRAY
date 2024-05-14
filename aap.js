176865e54373017c658e1375c514e54d29e2d469
NomanGhaziani <153339067+NomanGhaziani@users.noreply.github.com>
2024-05-14 16:41:46 +0500

    Add files via upload

---
+++ aap.js
@@ -0,0 +1,9 @@
+var fruits = ["apple", "banana", "mango", "orange",];
+fruits.pop();
+fruits.push('cherry');
+fruits.shift();
+fruits.unshift('waterMellon');
+fruits.slice();
+fruits.splice(1);
+console.log(fruits);
+//console.log(fruits[3]);
